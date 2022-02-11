6b54fc7f02548633e76d4baedd96d8bb30b3e359
johnkyle <johnkyle@umich.edu>
2022-02-11 13:24:06 -0500

    updates

--- insta485/api/helper.py
+++ insta485/api/helper.py
@@ -9,8 +9,23 @@ for good requests, I think 200 for return content, 204 for good delete, but chec
 import flask
 import insta485
 
+def verify_user(username, password):
+    """Takes the given username and password and verifies."""
+    print('implement me')
+
 def check_authentication():
     """Function to handle the authentication of the user."""
-    # needs to work with session and authen. with http
-    print('implement me!')
-    # NOTE: return 403 if the credentials don't work out
+    # get flask session stuff
+    session_username = flask.request.form.get('username')
+    session_password = flask.request.form.get('password')
+    if session_username and session_password:
+        # do flask stuff
+        print('implement me')
+
+    # get http basic authentification stuff
+    http_username = flask.request.authorization.get('username')
+    http_password = flask.request.authorization.get('password')
+    if http_username and http_password:
+        # do http stuff
+        print('implement me')
+    
