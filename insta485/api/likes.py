183352f843488be92136c8ab8b51ceae28db022f
johnkyle <johnkyle@umich.edu>
2022-02-12 14:39:49 -0500

    2/12

--- insta485/api/likes.py
+++ insta485/api/likes.py
@@ -1,21 +1,14 @@
 """Handles like endpoints."""
 """
-FLASK RESPONSE CODES! 
-bad requests should take form of {'message': 'what was wrong', 'code': flaskcode}
-for good requests, I think 200 for return content, 204 for good delete, but check spec
+URLs handled in this file:
+/api/v1/likes/, args ?postid=<postid>, POST
+/api/v1/likes/<likeid>/, DELETE
 """
 import sqlite3
 import flask
 import insta485
 from insta485.api.helper import check_authentication
 
-
-"""
-URLs handled in this file.
-
-/api/v1/likes/ , args ?postid=<postid>, POST
-/api/v1/likes/<likeid>/, DELETE
-"""
 
 @insta485.app.route('/api/v1/likes/', methods=['POST'])
 def make_like():
@@ -45,11 +38,36 @@ def make_like():
     }
     return flask.jsonify(**context), 200
 
+
 @insta485.app.route('/api/v1/likes/', methods=['DELETE'])
 def delete_like():
     """Delete a like on specified post."""
     check_authentication()
-    postid = flask.request.args.get('postid')
-    if not postid:
+    likeid = flask.request.args.get('postid')
+    if not likeid:
         # FIXME: what to do if an arg is not specified
         return flask.jsonify(**{'message': 'not found'}), 404
+    # connect to db
+    connection = insta485.model.get_db()
+    # get the like they are requesting
+    like = connection.execute(
+        'SELECT L.owner '
+        'FROM likes L '
+        'WHERE L.likeid = ? ',
+        (likeid,)
+    ).fetchall()
+    # check if the like exists
+    if len(like) != 1:
+        return flask.jsonify(**{'message': 'not found'}), 404
+    # check if they own the like
+    if flask.session.get('logname') != like[0]['owner']:
+        return flask.jsonify(**{'message': 'you do not own this!'}), 403
+    # if it exists and they own it, delete it
+    connection.execute(
+        'DELETE FROM likes '
+        'WHERE likeid = ? ',
+        (likeid,)
+    )
+    # commit changes
+    connection.commit()
+    return flask.jsonify(**{'message': 'NO CONTENT'}), 204
