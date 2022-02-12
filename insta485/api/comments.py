e890e46ba0cd6955402bd9d05536c1ce79e377aa
johnkyle <johnkyle@umich.edu>
2022-02-12 14:52:27 -0500

    updated api __init__.py

--- insta485/api/comments.py
+++ insta485/api/comments.py
@@ -1,32 +1,40 @@
 """URLs for comments."""
+
 """
 FLASK RESPONSE CODES! 
 bad requests should take form of {'message': 'what was wrong', 'code': flaskcode}
 for good requests, I think 200 for return content, 204 for good delete, but check spec
 """
 
-
+import sqlite3
 import flask
 import insta485
 from helper import check_authentication
 
 @insta485.app.route('/api/v1/comments/', methods=['POST'])
 def make_comment():
+    """Make a comment on the specified post."""
     check_authentication()
     postid = flask.request.args['postid']
-    # use SELECT last_insert_rowid() to get the last id
-    # this isn't for incrementing, the return of this post
-    # needs to include all the comment information
-    # which means we need the comment id
-    # so, use that function to get the id and return the json
-    # then, we can make the comment, and the new id should match
-    # the one we need
+    # connect to db
+    connection = insta485.model.get_db()
+    connection.row_factory = sqlite3.Row
+    # get last commentid
+    last_commentid = connection.execute(
+        'SELECT last_insert_rowid() '
+        'FROM comments '
+    ).fetchall()
+    if len(last_commentid) == 0:
+        commentid = 1
+    else:
+        commentid = last_commentid[0]['last_insert_rowid()']
+    
 
 
 @insta485.app.route('/api/v1/comments/<commentid>/', method=['DELETE'])
 def delete_comment():
+    """Delete a comment."""
     # NOTE: if the commentid does not exist, return flask.abort(404)
     # NOTE: if the comment owner is not the logname, return flask.abort(403)
-    # NOTE:
     check_authentication()
     commentid = flask.request.args['commentid']
